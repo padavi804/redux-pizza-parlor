68729a479cba8eb9ad9edf45f14ab885362966c1
Patrick Davison <davisonpatrickj@gmail.com>
2024-06-22 12:58:11 -0500

    axios post created

--- src/components/App/Customer/Customer.jsx
+++ src/components/App/Customer/Customer.jsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Radio from '@mui/material/Radio';
@@ -10,14 +12,48 @@ import Button from '@mui/material/Button';
 
 
 function Customer() {
+
+let [customerName, setCustomerName] = useState('');
+let [customerAddress, setCustomerAddress] = useState('');
+let [customerCity, setCustomerCity] = useState('');
+let [customerZip, setCustomerZip] = useState('');
+
+const addCustomer = (evt) => {
+    evt.preventDefault();
+    console.log(`Adding order for ${customerName} is being added`)
+
+    axios({
+        method: 'POST',
+        url:'/api/order',
+        data: {
+            customer_name: customerName,
+            street_address: customerAddress,
+            city: customerCity,
+            zip: customerZip
+        }
+    })
+        .then(response => {
+            console.log('Customer info received', response);
+            // fetchList(); set GET route info when merged
+            setCustomerName('');
+            setCustomerAddress('');
+            setCustomerCity('');
+            setCustomerZip('');
+    })
+        .catch(error => {
+            alert('Customer information not received');
+            console.log(error);
+        })
+
+}
+
+
     return (
         <div>
             <h2>Step 2: Customer Information</h2>
             <Box
                 component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
+                sx={{'& > :not(style)': { m: 1, width: '25ch' },}}
                 noValidate
                 autoComplete="off"
             >
