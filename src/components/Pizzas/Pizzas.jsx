b58bbc904831bc7cab11ef699f33d201950b30e1
Alibashir Ahmed <alibashirahmed26@gmail.com>
2024-07-01 08:56:16 -0500

    add header to customer and pizza

--- src/components/Pizzas/Pizzas.jsx
+++ src/components/Pizzas/Pizzas.jsx
@@ -1,20 +1,22 @@
+import Header from '../Header/Header';
 import PizzaCard from '../PizzaCard/PizzaCard';
 
 function Pizzas(props) {
     let pizzaList = props.pizzaList;
 
     return (
+
         <>
-            
+            <Header />
             {
-                
+
                 pizzaList.map(
                     (pizza) => (
                         <PizzaCard pizza={pizza} />
                     )
                 )
             }
-            
+
         </>
     )
 }
