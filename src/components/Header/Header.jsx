61d3edfa8704f0aa858ff3d7ecffaf692f92dc38
Alibashir Ahmed <alibashirahmed26@gmail.com>
2024-07-01 08:55:26 -0500

    created header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+
+function Header() {
+  
+  return (
+    <header className="App-header">
+      <h1 className="App-title">Prime Pizza</h1>
+    
+      <p>Total: $25</p>
+    </header>
+  );
+}
+
+export default Header;
